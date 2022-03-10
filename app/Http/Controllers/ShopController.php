96d5440fdf515c03d5fa1af767e8892404d61c35
Yunus Elçi <56027479+yunuselci@users.noreply.github.com>
2022-03-10 14:27:31 +0300

    Update ShopController.php

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -12,7 +12,7 @@ class ShopController extends Controller
     public function index()
     {
         $shops = Shop::with('listings')->get();
-        return view('main.include.shops',compact('shops'));
+        return view('main.include.shops', compact('shops'));
     }
 
     public function create()
@@ -20,11 +20,9 @@ class ShopController extends Controller
         $userId = auth()->id();
 
         $shops = Shop::whereUserId($userId)->get();
-        foreach ($shops as $shop){
-            if($shop){
-                if(!is_null($shop->status)){
-                    return redirect()->route('dashboard')->with('error','Mevcut mağaza açma talebiniz bulunmaktadır, yönetici onayı bekleniyor.');
-                }
+        foreach ($shops as $shop) {
+            if ($shop && !is_null($shop->status)) {
+                return redirect()->route('dashboard')->with('error', 'Mevcut mağaza açma talebiniz bulunmaktadır, yönetici onayı bekleniyor.');
             }
         }
         return view('main.include.shop-create');
@@ -33,27 +31,26 @@ class ShopController extends Controller
     public function store(ShopPostRequest $request)
     {
         $userId = auth()->id();
-        if(!is_null($request->image)){
-            $imageName = "iSotStore_".rand(0,10000).".".$request->image->getClientOriginalExtension();
+        if (!is_null($request->image)) {
+            $imageName = "iSotStore_" . rand(0, 10000) . "." . $request->image->getClientOriginalExtension();
             $request->image->move(public_path('main/images/shop'), $imageName);
-        }else{
+        } else {
             $imageName = $request->image;
         }
         $shop = Shop::create([
             'name' => $request->name,
-            'user_id'=> $userId,
-            'email'=> $request->email,
+            'user_id' => $userId,
+            'email' => $request->email,
             'phone' => $request->phone,
             'address' => $request->address,
             'about' => $request->about,
             'image' => $imageName
         ]);
-        if($shop)
-        {
-            return redirect()->route('dashboard')->with('success','Mağaza oluşturma isteği gönderildi, yetkililer tarafından incelentikten sonra onaylanacaktır.');
+        if ($shop) {
+            return redirect()->route('dashboard')->with('success', 'Mağaza oluşturma isteği gönderildi, yetkililer tarafından incelentikten sonra onaylanacaktır.');
 
-        }else{
-            return redirect()->route('magazalar.create')->with('error','Mağaza oluşturulurken bir hata meydana geldi.');
+        } else {
+            return redirect()->route('magazalar.create')->with('error', 'Mağaza oluşturulurken bir hata meydana geldi.');
 
         }
 
@@ -63,9 +60,9 @@ class ShopController extends Controller
     public function show($id)
     {
         $shop = Shop::with('listings')->whereId($id)->get();
-        if($shop){
-            return view('main.include.shop-page',compact('shop'));
-        }else{
+        if ($shop) {
+            return view('main.include.shop-page', compact('shop'));
+        } else {
             abort(404);
         }
 
@@ -74,11 +71,11 @@ class ShopController extends Controller
     public function edit($id)
     {
         $shop = Shop::find($id);
-        if($shop){
+        if ($shop) {
             $shop = Shop::find($id)->get();
 
-            return view('main.include.shop-edit',compact('shop'));
-        }else{
+            return view('main.include.shop-edit', compact('shop'));
+        } else {
             abort(404);
         }
     }
@@ -86,28 +83,27 @@ class ShopController extends Controller
     public function update(ShopPostRequest $request, $id)
     {
         $userId = auth()->id();
-        if(!is_null($request->image)){
-            $imageName = "iSotStore_".rand(0,10000).".".$request->image->getClientOriginalExtension();
+        if (!is_null($request->image)) {
+            $imageName = "iSotStore_" . rand(0, 10000) . "." . $request->image->getClientOriginalExtension();
             $request->image->move(public_path('main/images/shop'), $imageName);
-        }else{
+        } else {
             $imageName = Shop::find($id)->image;
         }
         $isShop = Shop::whereId($id);
-        if($isShop){
+        if ($isShop) {
             Shop::whereId($id)->update([
                 'name' => $request->name,
-                'user_id'=> $userId,
-                'email'=> $request->email,
+                'user_id' => $userId,
+                'email' => $request->email,
                 'phone' => $request->phone,
                 'address' => $request->address,
                 'about' => $request->about,
                 'image' => $imageName
 
             ]);
-            return redirect()->route('magazalar.edit',$id)->with('success','Mağaza bilgileri başarıyla kaydedildi.');
-        }
-        else{
-            return redirect()->route('magazalar.edit',$id)->with('error','Bir hata meydana geldi.');
+            return redirect()->route('magazalar.edit', $id)->with('success', 'Mağaza bilgileri başarıyla kaydedildi.');
+        } else {
+            return redirect()->route('magazalar.edit', $id)->with('error', 'Bir hata meydana geldi.');
 
         }
 
