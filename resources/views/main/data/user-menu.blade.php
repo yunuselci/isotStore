d4a4c41c625f2757cef72556137babddd5661ddc
Yunus Elçi <56027479+yunuselci@users.noreply.github.com>
2022-03-12 16:23:16 +0300

    Boilerplate codes moved to the data folder

---
+++ resources/views/main/data/user-menu.blade.php
@@ -0,0 +1,68 @@
+<div class="fixed-bar fl-wrap">
+    <div class="user-profile-menu-wrap fl-wrap">
+        <!-- user-profile-menu-->
+        <div class="user-profile-menu">
+            <h3>Ana Bölüm</h3>
+            <ul>
+                <li><a href="{{ route('dashboard') }}"
+                       class="{{ Route::currentRouteName() === 'dashboard' ? 'user-profile-act' : '' }}"><i
+                            class="fa fa-user"></i>Kullanıcı paneli</a></li>
+                <li><a href="{{ route('profile.show') }}"
+                       class="{{ Route::currentRouteName() === 'profile.show' ? 'user-profile-act' : '' }}"><i
+                            class="fa fa-edit"></i>Profili düzenle</a></li>
+            </ul>
+        </div>
+        <!-- user-profile-menu end-->
+        <!-- user-profile-menu-->
+        <div class="user-profile-menu">
+            <h3>Mağazam</h3>
+            <ul>
+                @foreach(Auth::user()->whereId(Auth::id())->with('shops')->get() as $value)
+                    @if(!is_null($value->shops))
+                        @if($value->shops->first()->status == 1)
+                            <li><a href="{{ route('magazalar.show', $value->shops->first()->id) }}"
+                                   class="{{ Route::currentRouteName() === 'magazalar.show' ? 'user-profile-act' : '' }}"><i
+                                        class="fa fa-shopping-bag"></i> Mağazam </a></li>
+                            <li><a href="{{ route('teklifler.show',$value->shops->first()->id ) }}"
+                                   class="{{ Route::currentRouteName() === 'teklifler.show' ? 'user-profile-act' : '' }}"><i
+                                        class="fa fa-envelope-o"></i> Teklifler</a></li>
+
+                            <li><a href="{{ route('magazalar.edit',$value->shops->first()->id) }}"
+                                   class="{{ Route::currentRouteName() === 'magazalar.edit' ? 'user-profile-act' : '' }}"><i
+                                        class="fa fa-edit"></i>Mağaza düzenle</a></li>
+                        @else
+                            <li><a class="user-profile-act"><i class="fa fa-warning"></i>Onay bekliyor</a></li>
+                        @endif
+                    @else
+                        <li><a href="{{ route('magazalar.create') }}"
+                               class="{{ Route::currentRouteName() === 'magazalar.create' ? 'user-profile-act' : '' }}"><i
+                                    class="fa fa-shopping-bag"></i>Mağaza oluştur</a></li>
+                    @endif
+                @endforeach
+            </ul>
+        </div>
+        <!-- user-profile-menu end-->
+        <!-- user-profile-menu-->
+        <div class="user-profile-menu">
+            <h3>Listelemeler</h3>
+            <ul>
+                <li><a href="{{ route('ilanlar.show',$value->shops->first()->id) }}"
+                       class="{{ Route::currentRouteName() === 'ilanlar.show' ? 'user-profile-act' : '' }}"><i
+                            class="fa fa-th-list"></i>İlanlarım </a></li>
+                <li><a href="{{ route('ilanlar.create') }}"
+                       class="{{ Route::currentRouteName() === 'ilanlar.create' ? 'user-profile-act' : '' }}"><i
+                            class="fa fa-plus-square-o"></i>Yenisini Ekle</a></li>
+            </ul>
+        </div>
+        <!-- user-profile-menu end-->
+        <form method="POST" action="{{ route('logout') }}">
+            @csrf
+
+            <button class="log-out-btn" type="submit"
+                    onclick="event.preventDefault();
+                                                     this.closest('form').submit();">
+                Çıkış Yap
+            </button>
+        </form>
+    </div>
+</div>
--- resources/views/main/include/offer/offers.blade.php
+++ resources/views/main/include/offer/offers.blade.php
@@ -17,61 +17,8 @@
                     </div>
                     <div class="row">
                         <div class="col-md-3">
-                            <div class="fixed-bar fl-wrap">
-                                <div class="user-profile-menu-wrap fl-wrap">
-                                    <!-- user-profile-menu-->
-                                    <div class="user-profile-menu">
-                                        <h3>Ana Bölüm</h3>
-                                        <ul>
-                                            <li><a href="{{ route('dashboard') }}"><i class="fa fa-user"></i>Kullanıcı paneli</a></li>
-                                            <li><a href="{{ route('profile.show') }}"><i class="fa fa-edit"></i>Profili düzenle</a></li>
-                                        </ul>
-                                    </div>
-                                    <!-- user-profile-menu end-->
-                                    <!-- user-profile-menu-->
-                                    @foreach(Auth::user()->whereId(Auth::id())->with('shops')->get() as $value)
-                                        <div class="user-profile-menu">
-                                            <h3>Mağazam</h3>
-                                            <ul>
-                                                @if(!is_null($value->shops))
-                                                    @if($value->shops->first()->status == 1)
-                                                        <li><a href="{{ route('magazalar.show', $value->shops->first()->id) }}"><i class="fa fa-shopping-bag"></i> Mağazam </a></li>
-                                                        <li><a href="{{ route('teklifler.show', $value->shops->first()->id) }}"  class="user-profile-act"><i class="fa fa-envelope-o"></i> Teklifler </a></li>
+                            @include('main.data.user-menu')
 
-                                                        <li><a href="{{ route('magazalar.edit',$value->shops->first()->id) }}"><i class="fa fa-edit"></i>Mağaza düzenle</a></li>
-                                                    @else
-                                                        <li><a class="user-profile-act"><i class="fa fa-warning"></i>Onay bekliyor</a></li>
-                                                    @endif
-                                                @else
-                                                    <li><a href="{{ route('magazalar.create') }}"><i class="fa fa-shopping-bag"></i>Mağaza oluştur</a></li>
-                                                @endif
-                                            </ul>
-                                        </div>
-                                        <!-- user-profile-menu end-->
-                                        <!-- user-profile-menu-->
-                                        @if(!is_null($value->shops) && $value->shops->first()->status == 1)
-                                            <div class="user-profile-menu">
-                                                <h3>Listelemeler</h3>
-                                                <ul>
-                                                    <li><a href="{{ route('ilanlar.show',$value->shops->first()->id) }}"><i class="fa fa-th-list"></i>İlanlarım </a></li>
-                                                    <li><a href="{{ route('ilanlar.create') }}"><i class="fa fa-plus-square-o"></i>Yenisini Ekle</a></li>
-                                                </ul>
-                                            </div>
-                                    @endif
-
-                                @endforeach
-                                <!-- user-profile-menu end-->
-                                    <form method="POST" action="{{ route('logout') }}">
-                                        @csrf
-
-                                        <button class="log-out-btn"  type="submit"
-                                                onclick="event.preventDefault();
-                                                     this.closest('form').submit();">
-                                            Çıkış Yap
-                                        </button>
-                                    </form>
-                                </div>
-                            </div>
                         </div>
 
                         <div class="col-md-9">
@@ -116,6 +63,6 @@
         </section>
         <!-- section end -->
         <div class="limit-box fl-wrap"></div>
-        
+
     </div>
 @endsection
